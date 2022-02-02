37e6a5c2b6e36e18da273d253da85fb1d909685a
Yves Faems <yves.faems@gmail.com>
2022-02-01 22:38:50 -0800

    registering model

--- app/sev/settings.py
+++ app/sev/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'vocab.apps.VocabConfig',
+    'vocab.apps.VocabConfig',
 ]
 
 MIDDLEWARE = [
