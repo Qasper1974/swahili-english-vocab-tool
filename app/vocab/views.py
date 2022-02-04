a0a342669d965bd13b03aad2853a5e283e25dfec
Yves Faems <yves.faems@gmail.com>
2022-02-03 21:29:12 -0800

    home page

--- app/vocab/views.py
+++ app/vocab/views.py
@@ -8,6 +8,6 @@ def home(request):
     return render(request, 'home.html')
 
 def ste(request):
-    voc = Vocabulary.objects.all()
+    voc = Vocabulary.objects.order_by('swahili_word')
     context = {'voc' : voc}
     return render(request, 'swahili_to_english.html', context)
