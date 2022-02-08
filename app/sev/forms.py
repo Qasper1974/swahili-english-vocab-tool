bc4d46ad8cd52c85425771f3fa68f05861dfa58e
Yves Faems <yves.faems@gmail.com>
2022-02-07 18:20:25 -0800

    remove superfluous forms and models

--- app/sev/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from .models import Vocabulary
-
-class VocabularyForm(ModelForm):
-    class Meta:
-        model = Vocabulary
-        fields = '__all__'
--- app/sev/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Vocabulary(models.Model):
-    swahili_word = models.CharField(max_length=55, blank=False, default='')
-    english_translation = models.CharField(max_length=55, blank=False, default='')
-    lesson = models.IntegerField()
-    known = models.BooleanField(default=False)
