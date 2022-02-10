3f7d43742880fc6995604fa6501d1c812b6cd180
Yves Faems <yves.faems@gmail.com>
2022-02-10 00:16:09 -0800

    add filter for english and swahili words

--- app/vocab/filters.py
+++ app/vocab/filters.py
@@ -1,9 +1,13 @@
 import django_filters
 
+from django_filters import CharFilter
+
 from .models import Vocabulary
 
 
 class WordFilter(django_filters.FilterSet):
+    SwahiliWord = CharFilter(field_name='swahili_word', lookup_expr='icontains')
+    EnglishTranslation = CharFilter(field_name='english_translation', lookup_expr='icontains')
     class Meta:
         model = Vocabulary
-        fields = ['swahili_word', 'english_translation', 'lesson']
+        fields = ['lesson']
